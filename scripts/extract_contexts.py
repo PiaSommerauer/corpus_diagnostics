d5da0aa1bea328fe6f5550f122317b00498a8c71
Pia Sommerauer <pia.sommerauer@live.com>
2021-06-02 09:47:14 +0200

    multiprocessing tested locally

--- scripts/extract_contexts.py
+++ scripts/extract_contexts.py
@@ -4,10 +4,10 @@ from tqdm import tqdm
 import os
 import sys
 from collections import defaultdict
-#from numba import jit
+import multiprocessing
+from collections import defaultdict
 
-from sklearn.feature_extraction.text import TfidfVectorizer as tfidf
-
+import itertools
 
 def load_data(prop):
     path = '../data/aggregated/'+prop+'.json'
@@ -34,82 +34,99 @@ def load_pairs(path):
     return pairs
 
 
-def to_file(contexts, prop, target, label):
-    
-    path_dir = '../contexts'
-    if not os.path.isdir(path_dir):
-        os.mkdir(path_dir)
-    path_dir = '../contexts/'+prop
-    if not os.path.isdir(path_dir):
-        os.mkdir(path_dir)
-    path_dir = '../contexts/'+prop+'/'+label
-    if not os.path.isdir(path_dir):
-        os.mkdir(path_dir)
-    contexts_str = ' '.join(contexts)
-    with open(path_dir+'/'+target+'.txt', 'w') as outfile:
-        outfile.write(contexts_str)
 
-
-def extract_contexts(prop_targets, pair_line, target_contexts):
+def extract_contexts(qu, prop_targets, pair_line):
 
     pair = pair_line.strip().split(' ')
     w, c = pair
+    target_contexts= defaultdict(dict)
     for prop, label_dict in prop_targets.items():
         for label, targets in label_dict.items():
-            target_contexts[prop][label] = dict()
+            target_contexts[prop][label] = defaultdict(list)
             for target in targets:
                 if target == w:
-                    if target in target_contexts[prop][label]:
-                        target_contexts[prop][label][target].append(c)
-                    else:
-                        target_contexts[prop][label][target] = [c]
+                    target_contexts[prop][label][target].append(c)
+    qu.put(target_contexts)
 
-                        
-                        
+
+
+def get_batches(n_lines, n_batches):
+    batch_size = round(n_lines / n_batches)
+    batches = []
+    start = 0
+    for n in range(n_batches):
+        end = start + batch_size
+        batches.append((start, end))
+        start = end
+    # add final batch
+    last_b = batches[-1]
+    end = last_b[1]
+    if end < n_lines:
+        batches.append((end, n_lines + 1))
+    return batches
                         
 def main():
-    
-    #prop = sys.argv[1]
+
+
     prop = sys.argv[1]
     path_dir = sys.argv[2]
-
-
-    #path_dir = '/Users/piasommerauer/Data/dsm/corpus_exploration/wiki_full/run.2017-03-20-10:31:44'
-    #test_vocab = f'{test_path_dir}/counts.words.vocab'
+    model_name = sys.argv[3]
     pair_path =  f'{path_dir}/pairs-orig'
 
     # for progress bar  - use wc -l on terminal to get line number of pairs file
-    n_lines = 1530697448
-
-
+    #n_lines = 1530697448
+    n_lines = 6
+    #n_batches = 1000000
+    n_batches = 1
+    batches = get_batches(n_lines, n_batches)
 
     props = [prop]
-    labels = ['pos', 'neg']
     prop_targets = dict()
-    target_contexts = dict()
+    all_contexts = dict()
     for prop in props:
+        all_contexts[prop] = dict()
         prop_dict = load_data(prop)
         target_pos = [k for k, d in prop_dict.items() if d['ml_label'] in ['all', 'all-some', 'few-some']]
         target_neg = [k for k, d in prop_dict.items() if d['ml_label'] in ['few']]
         prop_targets[prop] = dict()
-        target_contexts[prop] = dict()
         prop_targets[prop]['pos'] = target_pos
         prop_targets[prop]['neg'] = target_neg
-        for label in labels:
-            target_contexts[prop][label] = dict()
+        all_contexts[prop]['pos'] = defaultdict(list)
+        all_contexts[prop]['neg'] = defaultdict(list)
 
-    with open(pair_path) as infile:
-        for pair_line in tqdm(infile, total=n_lines):
-            extract_contexts(prop_targets, pair_line, target_contexts)
+    batch_cnt = 0
+    for start, end in batches:
+        #output_dicts_batch = []
+        with open(pair_path) as infile:
+            my_lines =  itertools.islice(infile, start, end)
+            jobs = []
+            qu = multiprocessing.Queue()
+            for pair_line in my_lines:
+                p = multiprocessing.Process(target=extract_contexts,
+                                            args=(qu, prop_targets, pair_line))
+                jobs.append(p)
+                p.start()
 
-    path_dir = '../contexts/' + prop + '/' + label
-    os.makdirs(path_dir, exist_ok=True)
-    for prop, label_dict in target_contexts.items():
+            for proc in jobs:
+                out = qu.get()
+                for prop, label_dict in out.items():
+                    for label, target_dict in label_dict.items():
+                        for target, contexts in target_dict.items():
+                            all_contexts[prop][label][target].extend(contexts)
+                proc.join()
+        batch_cnt += 1
+        print(all_contexts)
+        if batch_cnt == 3:
+            break
+
+
+
+    for prop, label_dict in all_contexts.items():
         for label, target_dict in label_dict.items():
+            path_dir = f'../contexts/{model_name}/{prop}/{label}'
+            os.makedirs(path_dir, exist_ok=True)
             for target, contexts in target_dict.items():
-                path_dir = '../contexts/' + prop + '/' + label
-                os.makdirs(path_dir, exist_ok=True)
-                with open(path_dir+'/'+target+'.txt', 'w') as outfile:
+                with open(f'{path_dir}/{target}.txt', 'w') as outfile:
                     outfile.write(' '.join(contexts))
 
 if __name__ == '__main__':
--- scripts/test_multi.py
+++ scripts/test_multi.py
@@ -1,24 +1,32 @@
 import multiprocessing
 
-def worker(procnum, qu):
-    ret = qu.get()
-    print(str(procnum) + ' represent!')
-    ret.append(procnum)
-    qu.put(ret)
+
+def test_process(qu, v):
+    d = dict()
+    search = [1,3,5]
+    for s in search:
+        if v == s:
+            d[s]=v
+    qu.put(d)
 
 
 if __name__ == '__main__':
     jobs = []
+
     qu = multiprocessing.Queue()
-    ret = []
+
+    output = []
+    my_list = []
+    print('start multi')
     for i in range(5):
-        p = multiprocessing.Process(target=worker, args=(i,qu))
+        p = multiprocessing.Process(target=test_process, args=(qu, i))
         jobs.append(p)
         p.start()
 
-    results = []
     for proc in jobs:
+        out = qu.get()
+        output.append(out)
         proc.join()
-        ret = qu.get()
-        results.append(ret)
-    print(results)
+
+
+    print(output)
