289e50be6535a465a60863c12b78e953459a22ee
PiaSommerauer <pia.sommerauer@live.com>
2021-06-03 06:21:06 +0000

    multiprocess modified

--- scripts/extract_contexts.py
+++ scripts/extract_contexts.py
@@ -6,7 +6,7 @@ import sys
 from collections import defaultdict
 import multiprocessing
 from collections import defaultdict
-
+import time
 import itertools
 
 def load_data(prop):
@@ -35,8 +35,8 @@ def load_pairs(path):
 
 
 
-def extract_contexts(qu, prop_targets, pair_line):
-
+def extract_contexts(prop_targets_pair_line):
+    prop_targets, pair_line = prop_targets_pair_line
     pair = pair_line.strip().split(' ')
     w, c = pair
     target_contexts= defaultdict(dict)
@@ -46,7 +46,7 @@ def extract_contexts(qu, prop_targets, pair_line):
             for target in targets:
                 if target == w:
                     target_contexts[prop][label][target].append(c)
-    qu.put(target_contexts)
+    return target_contexts
 
 
 
@@ -68,19 +68,23 @@ def get_batches(n_lines, n_batches):
 def main():
 
 
-    prop = sys.argv[1]
-    path_dir = sys.argv[2]
-    model_name = sys.argv[3]
+    #prop = sys.argv[1]
+    path_dir = sys.argv[1]
+    model_name = sys.argv[2]
     pair_path =  f'{path_dir}/pairs-orig'
 
     # for progress bar  - use wc -l on terminal to get line number of pairs file
-    #n_lines = 1530697448
-    n_lines = 6
-    #n_batches = 1000000
-    n_batches = 1
+    n_lines = 1530697448
+    n_batches = 1000
+
+    # test:
+    #n_batches = 1
+    # n_lines = 6
+
     batches = get_batches(n_lines, n_batches)
 
-    props = [prop]
+    #props = ['blue', 'green']
+    props = ['lay_eggs', 'yellow', 'used_in_cooking']
     prop_targets = dict()
     all_contexts = dict()
     for prop in props:
@@ -95,29 +99,38 @@ def main():
         all_contexts[prop]['neg'] = defaultdict(list)
 
     batch_cnt = 0
+
     for start, end in batches:
+        print(f'processing batch {batch_cnt} of {len(batches)}')
+        print(f'batch size: {end-start}')
         #output_dicts_batch = []
         with open(pair_path) as infile:
             my_lines =  itertools.islice(infile, start, end)
-            jobs = []
-            qu = multiprocessing.Queue()
-            for pair_line in my_lines:
-                p = multiprocessing.Process(target=extract_contexts,
-                                            args=(qu, prop_targets, pair_line))
-                jobs.append(p)
-                p.start()
+            # create arguments:
+            #arguments = []
+            #qu = multiprocessing.Queue()
+            #for pair_line in my_lines:
+             #   arguments.append([qu, prop_targets, pair_line])
+            #jobs = []
+            #po = multiprocessing.Pool(4)
+            po = multiprocessing.Pool(5)
+                # extract_contexts(prop_targets, pair_line)
+            out = po.map(extract_contexts, [(prop_targets, pair_line) for pair_line in my_lines])
 
-            for proc in jobs:
-                out = qu.get()
-                for prop, label_dict in out.items():
+
+            for prop_target_dict in out:
+                for prop, label_dict in prop_target_dict.items():
                     for label, target_dict in label_dict.items():
                         for target, contexts in target_dict.items():
                             all_contexts[prop][label][target].extend(contexts)
-                proc.join()
-        batch_cnt += 1
-        print(all_contexts)
-        if batch_cnt == 3:
-            break
+            batch_cnt += 1
+            po.close()
+            po.join()
+
+
+            #print(all_contexts)
+            if batch_cnt == 3:
+                break
 
 
 
@@ -130,7 +143,9 @@ def main():
                     outfile.write(' '.join(contexts))
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - start_time))
     
     
     
