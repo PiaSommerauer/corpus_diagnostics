885b9b21bce709bf6c8c5f6700069e20220e7b5e
PiaSommerauer <pia.sommerauer@live.com>
2021-06-23 11:58:05 +0000

    correct problems

--- scripts/extract_contexts.py
+++ scripts/extract_contexts.py
@@ -25,13 +25,13 @@ def load_vocab(path):
             vocab_counts[word] = cnt
     return vocab_counts
 
-def load_pairs(path):
-    pairs = []
-    with open(path) as infile:
-        for line in infile:
-            pair = line.strip().split(' ')
-            pairs.append(pair)
-    return pairs
+# def load_pairs(path):
+#     pairs = []
+#     with open(path) as infile:
+#         for line in infile:
+#             pair = line.strip().split(' ')
+#             pairs.append(pair)
+#     return pairs
 
 
 
@@ -39,7 +39,7 @@ def extract_contexts(targets_pair_line):
     targets, pair_line = targets_pair_line
     pair = pair_line.strip().split(' ')
     w, c = pair
-    target_contexts= defaultdict(list)
+    target_contexts=defaultdict(list)
     for target in targets:
         if target == w:
             target_contexts[target].append(c)
@@ -66,51 +66,52 @@ def main():
 
 
     #prop = sys.argv[1]
-    path_dir = sys.argv[1]
-    model_name = sys.argv[2]
+    path_dir = '../../data/volume_1/models/giga_full/counts'
+    model_name = 'giga_full'
     if model_name == 'test':
         pair_path = 'pairs-test'
     else:
-        pair_path =  f'{path_dir}/pairs-orig'
+        pair_path =  f'{path_dir}/pairs'
 
 
     # for progress bar  - use wc -l on terminal to get line number of pairs file
-    n_lines = 1530697448
-    #         1530697448
+    n_lines = 1886693907
     n_batches = 1000
 
-    if model_name == 'test':
-        n_lines = 100
-        n_batches = 10
+    # if model_name == 'test':
+    #     n_lines = 100
+    #     n_batches = 10
 
     # test:
     #n_batches = 1
     # n_lines = 6
 
     batches = get_batches(n_lines, n_batches)
+    print(f'created {len(batches)} batches.')
     path_vocab ='../data/vocab.txt'
     if model_name == 'test':
         path_vocab = '../data/vocab-test.txt'
     with open(path_vocab) as infile:
         targets = set(infile.read().strip().split('\n'))
+    print('loaded vocab of size: ', len(targets))
 
-    # get already collected:
-    already_collected = set()
-    collected_dir = f'../contexts/{model_name}/vocab/'
-    if os.path.isdir(collected_dir):
-        for f in os.listdir(collected_dir):
-            w = f.split('.')[0]
-            already_collected.add(w)
+    # # get already collected:
+    # already_collected = set()
+    # collected_dir = f'../contexts/{model_name}/vocab/'
+    # if os.path.isdir(collected_dir):
+    #     for f in os.listdir(collected_dir):
+    #         w = f.split('.')[0]
+    #         already_collected.add(w)
     # take 100
-    print('already collected contexts for:')
-    print(already_collected)
-    targets_remaining = list(targets.difference(already_collected))
-    print('Targets remaining:', len(targets_remaining))
-    vocab_size = 400
-    if len(targets_remaining) > vocab_size:
-        targets_to_collect = targets_remaining[:vocab_size]
-    else:
-        targets_to_collect = targets_remaining
+    # print('already collected contexts for:')
+    # print(already_collected)
+    # targets_remaining = list(targets.difference(already_collected))
+    # print('Targets remaining:', len(targets_remaining))
+    # vocab_size = 400
+    # if len(targets_remaining) > vocab_size:
+    #     targets_to_collect = targets_remaining[:vocab_size]
+    # else:
+    #     targets_to_collect = targets_remaining
     batch_cnt = 0
     outputs = []
     output_batches = 0
@@ -125,62 +126,54 @@ def main():
             print(f'batch size: {end-start}')
             #output_dicts_batch = []
 
-            my_lines =  itertools.islice(infile, start, end)
+            my_lines = itertools.islice(infile, start, end)
 
-            # use 3 cpus
-            po = multiprocessing.Pool(3)
+            # use 5 cpus
+            po = multiprocessing.Pool(10)
             #extract_contexts(targets_pair_line)
-            out = po.map(extract_contexts, [(targets_to_collect, pair_line) for pair_line in my_lines])
+            out = po.map(extract_contexts, [(targets, pair_line) for pair_line in my_lines])
 
 
             po.close()
             po.join()
-            outputs.extend(out)
+            #outputs.extend(out)
+            print(f'processed {len(out)} lines')
 
-            output_batches += 1
-            if output_batches == 3:
-                print('writing to file')
-                print(f'processing {len(outputs)} outputs')
-                n_contexts = 0
-                context_dict_all = dict()
-                for context_dict in outputs:
-                    for target, contexts in context_dict.items():
-                        n_contexts += len(contexts)
-                        if target not in context_dict_all:
-                            context_dict_all[target] = contexts
-                        else:
-                            context_dict_all[target].extend(contexts)
-                print('number of targets:', n_contexts)
-                output_batches = 0
-                outputs = []
-                path_dir = f'../contexts/{model_name}/vocab'
-                os.makedirs(path_dir, exist_ok=True)
-                for target, contexts in context_dict_all.items():
-                    path_target = f'{path_dir}/{target}.txt'
-                    if path_target in paths:
-                        mode = 'a'
-                    else:
-                        mode = 'w'
-                    with open(path_target, mode) as outfile:
-                        outfile.write('\n'.join(contexts)+'\n')
-                    paths.add(path_target)
+            print('writing to file')
+            print(f'processing {len(outputs)} outputs')
+            n_contexts = 0
+            context_dict_all = defaultdict(list)
+            for context_dict in out:
+                for target, contexts in context_dict.items():
+                    n_contexts += len(contexts)
+                    context_dict_all[target].extend(contexts)
+            print('number of targets:', n_contexts)
+            output_batches = 0
+            #outputs = []
+            # write to datablock instead of repo
+            #path_dir = f'../contexts/{model_name}/vocab'
+            path_dir = f'../../data/volume_1/contexts/{model_name}/vocab'
+            os.makedirs(path_dir, exist_ok=True)
+            for target, contexts in context_dict_all.items():
+                path_target = f'{path_dir}/{target}.txt'
+                if path_target in paths:
+                    mode = 'a'
+                else:
+                    mode = 'w'
+                with open(path_target, mode) as outfile:
+                    outfile.write('\n'.join(contexts)+'\n')
+                paths.add(path_target)
 
-            end_time = time.time()
-            dur = (end_time-start_time)/60 # min
-            total_dur += dur
-            mean_dur = total_dur/batch_cnt
-            remaining_batches = len(batches) - batch_cnt
-            est_dur = (remaining_batches * mean_dur )/ 60 #hours
-            print(f'Finished batch {batch_cnt}')
-            print(f'{remaining_batches} batches remaining')
-            print(f'This batch took {dur} minutes (mean duration: {mean_dur} minutes)')
-            print(f'Projected duration remaining: {est_dur} hours')
-
-
-
-
-
-
+        end_time = time.time()
+        dur = (end_time-start_time)/60 # min
+        total_dur += dur
+        mean_dur = total_dur/batch_cnt
+        remaining_batches = len(batches) - batch_cnt
+        est_dur = (remaining_batches * mean_dur )/ 60 #hours
+        print(f'Finished batch {batch_cnt}')
+        print(f'{remaining_batches} batches remaining')
+        print(f'This batch took {dur} minutes (mean duration: {mean_dur} minutes)')
+        print(f'Projected duration remaining: {est_dur} hours')
 
 
 
